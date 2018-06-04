1fd5853781e9761fd305352d19ebdf31b6772658
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2018-06-04 15:23:58 +0200

    Implement getDetails, getDescription and getDate for Observation and MedicationStatement

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ serverUrl = "http://localhost:8080/baseDstu3"
 class Observation {
     constructor(data) {
         this.data = data
+        this.date = new Date(this.data.issued)
     }
 
     getDescription() {
@@ -10,7 +11,42 @@ class Observation {
     }
 
     getDate() {
-        return this.data.issued
+        return this.date
+    }
+
+    getDetails() {
+        var field;
+        var def = "empty"
+        try {
+            field = this.data.valueCodeableConcept.text; 
+        }
+        catch (err) {
+            field = def;
+        }
+        return {
+            text: field
+        }
+    }
+}
+
+class MedicationStatement {
+    constructor(data) {
+        this.data = data
+        this.date = new Date(this.data.authoredOn)
+    }
+    
+    getDescription() {
+        return this.data.medicationCodeableConcept.text
+    }
+
+    getDate() {
+        return this.date
+    }
+    
+    getDetails() {
+        return {
+            text: this.data.extension[0].valueCodeableConcept.text
+        }
     }
 }
 
@@ -34,7 +70,7 @@ class Patient {
     getObservations() {
         var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         var xmlHttp = new XMLHttpRequest();
-        getPatientsUrl = serverUrl + "/Observation?patient=" + this.getId() + "&_sort=date&_format=json"
+        var getPatientsUrl = serverUrl + "/Observation?patient=" + this.getId() + "&_sort=date&_format=json"
         xmlHttp.open( "GET", getPatientsUrl, false ); // false for synchronous request
         xmlHttp.send(null);
         var entries = JSON.parse(xmlHttp.responseText).entry
@@ -43,13 +79,38 @@ class Patient {
         })
     }
 
-    getPatientDetails() {
+    getMedicationStatements() {
         var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-        var xmlHttp = new XMLHttpRequest();
-        getPatientsUrl = serverUrl + "/Patient/" + this.getId() +"/$everything/?_format=json"
-        xmlHttp.open( "GET", getPatientsUrl, false ); // false for synchronous request
+        var xmlHttp = new XMLHttpRequest(); 
+        var getMedicationsUrl = serverUrl + "/MedicationRequest?patient=" + this.getId() + "&_format=json" // sorting may be neccesary
+        xmlHttp.open( "GET", getMedicationsUrl, false ); // false for synchronous request
         xmlHttp.send(null);
-        return JSON.parse(xmlHttp.responseText)
+        var entries = JSON.parse(xmlHttp.responseText).entry
+        if(entries != null) {
+            return entries.map((e)=> {
+                var data = e.resource
+                return new MedicationStatement(data)
+            })
+        }
+        else {
+            return []
+        }
+        
+    }
+
+    getAllEvents() {
+        var medications = this.getMedicationStatements()
+        var observations = this.getObservations()
+        var all = medications.concat(observations)
+        all.sort((a, b) => {
+            if(a.date > b.date) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        })
+        return all
     }
 }
 
@@ -67,9 +128,13 @@ function getPatients() {
 
 patientList = getPatients()
 for(patient of patientList) {
+    var events = patient.getAllEvents()
     console.log(patient.getName(), patient.getSurname())
-    for (observation of patient.getObservations()) {
-        console.log(observation.getDate(), observation.getDescription())
+    for(event of events) {
+        var dateString = String(event.getDate())
+        var description = event.getDescription()
+        var details = event.getDetails()
+        console.log(dateString, ":", description)
+        console.log(details)
     }
-    
 }
