14e5d768f8d8d08e0446075db465cc8da5a03765
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2018-06-08 18:02:47 +0200

    [I] Implement third view - event details

--- src/viewComponents/EventView.js
+++ src/viewComponents/EventView.js
@@ -4,11 +4,15 @@ export class EventView extends Component {
   
   constructor(props) {
     super(props);
+    this.passDetails = this.passDetails.bind(this)
   }
 
+  passDetails() {
+    this.props.passDetails(this.props.details)
+  }
   render() {
     return (
-      <li> { this.props.date } { this.props.description } {this.props.details} </li>
+      <li onClick={ this.passDetails }> { this.props.date } { this.props.description } </li>
     );
   }
 }
--- src/viewComponents/EventsList.js
+++ src/viewComponents/EventsList.js
@@ -3,7 +3,7 @@ import { MedicationStatement, Observation } from '../helpers/resourceManager'
 import { EventView } from './EventView'
 
 export class EventsList extends Component {
-  
+
   constructor(props) {
     super(props);
 
@@ -12,36 +12,38 @@ export class EventsList extends Component {
 
     this.state = {
       stat: false,
-      obs: false
+      obs: false,
+      det: false
     };
 
     this.handleGoBack = this.handleGoBack.bind(this)
-  
+    this.showAll = this.showAll.bind(this)
     this.fetchObservations()
     this.fetchMedicationStatements()
+    this.passDetails = this.passDetails.bind(this)
   }
 
-  fetchObservations(){
+  fetchObservations() {
     fetch(this.props.serverUrl + "/Observation?patient=" + this.props.iden + "&_sort=date&_format=json")
       .then(blob => blob.json())
       .then(observations => {
         this.allObservations = observations.entry.map((observation) => {
           return new Observation(observation.resource)
         })
-        this.setState({ obs: true})
+        this.setState({ obs: true })
       })
       .catch(err => console.error(err));
   }
 
-  fetchMedicationStatements(){
+  fetchMedicationStatements() {
     fetch(this.props.serverUrl + "/MedicationRequest?patient=" + this.props.iden + "&_format=json")
       .then(blob => blob.json())
       .then(statements => {
-        if(statements != null){
+        if (statements != null) {
           this.allMedicationStatements = statements.entry.map((statement) => {
             return new MedicationStatement(statement.resource)
           })
-          this.setState({ stat: true})
+          this.setState({ stat: true })
         }
       })
       .catch(err => console.error(err));
@@ -52,36 +54,65 @@ export class EventsList extends Component {
     window.location.reload(true);
   }
 
+  passDetails(details) {
+    this.setState({
+      det: true,
+      content: details
+    });
+  }
+
+  showAll() {
+    this.setState({
+      det: false
+    });
+  }
+
   render() {
-    const observationList = this.allObservations.map((statement, index, array) => {
-        return <EventView key={ index } date={ statement.date } description={ statement.description } details={ statement.details } />;
+    let observationList;
+    let medicationStatementsList;
+    if (this.state.det == false) {
+      observationList = this.allObservations.map((statement, index, array) => {
+        return <EventView key={index} date={statement.date} description={statement.description} passDetails={this.passDetails} details={ statement.details } />;
+        /*return <EventView key={ index } date={ statement.date } description={ statement.description } details={ statement.details } />;*/
       });
 
-    const medicationStatementsList = this.allMedicationStatements.map((statement, index, array) => {
-        return <EventView key={ index + observationList.length } date={ statement.date } description={ statement.description } details={ statement.details } />;
+      medicationStatementsList = this.allMedicationStatements.map((statement, index, array) => {
+        return <EventView key={index + observationList.length} date={statement.date} description={statement.description} passDetails={this.passDetails} details={ statement.details } />
+        /*return <EventView key={ index + observationList.length } date={ statement.date } description={ statement.description } details={ statement.details } />;*/
       });
 
-    const all = observationList.concat(medicationStatementsList)
+      const all = observationList.concat(medicationStatementsList)
 
-    all.sort((a, b) => {
+      all.sort((a, b) => {
         let aDate = new Date(a.props.date);
         let bDate = new Date(b.props.date);
 
         if (aDate > bDate) {
-            return 1;
+          return 1;
         }
         else {
-            return -1;
+          return -1;
         }
-    })
+      })
 
-    return (
+      return (
         <div>
-        <h1> Patient's events: </h1>
-        <ul> { all } </ul>
-        <button type="button" onClick={ this.handleGoBack }>Go Back</button> 
+          <h1> Patient's events: </h1>
+          <ul> {all} </ul>
+          <button type="button" onClick={this.handleGoBack}>Go Back</button>
         </div>
-    );
+      );
+    }
+    else {
+      // wyswietlamy detailsy 
+      return (
+        <div>
+          <h1> Event's details: </h1>
+          <span> { this.state.content }</span>
+          <button type="button" onClick={this.showAll}>Go Back</button>
+        </div>
+      )
+    }
   }
 }
 
